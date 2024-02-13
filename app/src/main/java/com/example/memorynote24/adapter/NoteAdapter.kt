be3d016daed84f32900a5e538e392eb92836e03f
habilithm97 <habinkim97@gmail.com>
2024-02-13 18:04:34 +0900

    메모 수정하기

--- app/src/main/java/com/example/memorynote24/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/memorynote24/adapter/NoteAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.memorynote24.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
@@ -12,6 +13,7 @@ import com.example.memorynote24.room.Note
 
 // 데이터 클래스를 받음 -> 리스트 자체에서 데이터 리스트를 정의함
 class NoteAdapter : ListAdapter<Note, NoteAdapter.NoteViewHolder>(DiffCallback()) {
+    private lateinit var listener: OnItemClickListener // 전달된 리스너 객체를 저장할 변수
 
     class DiffCallback : DiffUtil.ItemCallback<Note>() {
         // 두 아이템이 같은 객체인지 확인
@@ -24,9 +26,17 @@ class NoteAdapter : ListAdapter<Note, NoteAdapter.NoteViewHolder>(DiffCallback()
         }
     }
 
-    class NoteViewHolder(val binding: ItemNoteBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class NoteViewHolder(private val binding: ItemNoteBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(note: Note) {
             binding.note = note
+
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                // 아이템 클릭 이벤트 핸들러 메서드에서 리스너 객체 메서드 호출
+                itemView.setOnClickListener {
+                    listener.onItemClick(itemView, note, position)
+                }
+            }
         }
     }
 
@@ -41,5 +51,14 @@ class NoteAdapter : ListAdapter<Note, NoteAdapter.NoteViewHolder>(DiffCallback()
 
     fun getPosition(position: Int) : Note {
         return getItem(position)
+    }
+
+    interface OnItemClickListener { // 커스텀 리스너 인터페이스
+        fun onItemClick(view: View, note: Note, position: Int)
+    }
+
+    // 리스너 객체를 전달하는 메서드
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
     }
 }
--- app/src/main/java/com/example/memorynote24/ui/MainActivity.kt
+++ app/src/main/java/com/example/memorynote24/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -26,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy { DataBindingUtil.setContentView<ActivityMainBinding>(this@MainActivity, R.layout.activity_main) }
     private val noteAdapter by lazy { NoteAdapter() }
     private val viewModel: NoteViewModel by viewModels() // ViewModelProvider를 사용하지 않고 viewModel 지연 생성 가능
-    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -105,6 +106,17 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        // 커스텀 리스너 객체 생성 및 전달
+        noteAdapter.setOnItemClickListener(object : NoteAdapter.OnItemClickListener {
+            override fun onItemClick(view: View, note: Note, position: Int) {
+                val intent = Intent(this@MainActivity, NoteActivity::class.java)
+                intent.putExtra(NoteActivity.ID, note.id)
+                intent.putExtra(NoteActivity.TITLE, note.title)
+                intent.putExtra(NoteActivity.CONTENT, note.content)
+                resultLauncher.launch(intent)
+            }
+        })
     }
 
     private fun deleteDialog(note: Note) {
